532758816aa210668af22d29cfc866c59b6d9675
ADBur23 <antburgess289@gmail.com>
2023-12-12 10:34:46 +0000

    added cycle to add past, present and future colours, and dispaly information

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Instructs engine to load the HTML and CSS before running the JS
 $(document).ready(function () {
-
   // Display current date and time
   var displayTime = document.querySelector("#currentDay");
 
@@ -8,13 +7,44 @@ $(document).ready(function () {
 
   displayTime.textContent = currentTime;
 
-    // SaveBtn click listener for user input
-    $(".saveBtn").on("click", function () {
-      var text = $(this).siblings(".description").val();
-      var time = $(this).parent().attr("id");
-  
-      // Save text in local storage
-      localStorage.setItem(time, text);
-    });
+  // SaveBtn click listener for user input
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
+    // Save text in local storage
+    localStorage.setItem(time, text);
+  });
+  function hourTracker() {
+    // Get current number of hours.
+    var currentHour = dayjs().hour();
+
+    // Loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // Check the time and add the classes for background colours
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  hourTracker();
+
+  // Function to loop over time blocks to retrieve and display data from local storage
+  function displayText() {
+    // Loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = $(this).attr("id");
+      $(this).children(".description").val(localStorage.getItem(blockHour));
+    });
+  }
+  displayText();
 });
